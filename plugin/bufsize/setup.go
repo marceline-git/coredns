cb734d665f8b803c7c58d2a99cef7c99e6c0ed1a
ykhr53 <yukihira.lab@gmail.com>
2019-10-31 18:23:59 +0100

    delete stdout messages when get an error
    Signed-off-by: ykhr53 <yukihira.lab@gmail.com>

--- plugin/bufsize/setup.go
+++ plugin/bufsize/setup.go
@@ -1,7 +1,6 @@
 package bufsize
 
 import (
-	"fmt"
 	"strconv"
 
 	"github.com/coredns/coredns/core/dnsserver"
@@ -37,18 +36,15 @@ func bufsizeParse(c *caddy.Controller) (int, error) {
 			// Specified value is needed to verify
 			bufsize, err := strconv.Atoi(args[0])
 			if err != nil {
-				fmt.Println("Invalid argument in bufsize.")
 				return -1, plugin.Error("bufsize", c.ArgErr())
 			}
 			// Follows RFC 6891
 			if bufsize < 512 || bufsize > 4096 {
-				fmt.Println("bufsize must be within 512 - 4096.")
 				return -1, plugin.Error("bufsize", c.ArgErr())
 			}
 			return bufsize, nil
 		default:
 			// Only 1 argument is acceptable
-			fmt.Println("bufsize accepts only 1 argument.")
 			return -1, plugin.Error("bufsize", c.ArgErr())
 		}
 	}
